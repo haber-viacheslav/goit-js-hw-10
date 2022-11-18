b492b20b047da1c1f2d24396783366d7bc2348f8
Viacheslav Haber <gaberslavik@gmail.com>
2022-11-18 19:06:31 +0200

    added changes

--- src/index.js
+++ src/index.js
@@ -21,15 +21,33 @@ function onSearch(e) {
   }
   fetchSearchedCountry(searchQuery).then(country => {
     console.log(country);
+    console.log(country[1].languages);
 
-    // refs.countriesList.innerHTML = murkup;
+    refs.countriesList.innerHTML = renderCountriesList(country);
   });
 }
 
 function fetchSearchedCountry(searchValue) {
-  return fetch(`https://restcountries.com/v3.1/name/${searchValue}`).then(
-    response => {
+  return fetch(`https://restcountries.com/v3.1/name/${searchValue}`)
+    .then(response => {
       return response.json();
-    }
-  );
+    })
+    .catch(error => console.log(error));
 }
+
+//{flags.svg, name.official, capital, population, languages}
+function renderCountriesList(countries) {
+  return countries
+    .map(country => {
+      return `<svg width='30' height='30'>
+    <use href='${country.flags.svg}'></use>
+    </svg>
+    <h2 class='country-title'>${country.name.official}</h2>
+    <p class='country-descr'>Capital:<span> ${country.capital}</span></p>
+    <p class='country-descr'>Polulation:<span> ${country.population}</span></p>
+    <p class='country-descr'>Languages:<span> ${country.languages}</span></p>`;
+    })
+    .join('');
+}
+
+function renderCountryCard() {}
